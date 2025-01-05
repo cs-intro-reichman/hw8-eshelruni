92adab9dc29bd22742c68dcd708a7a3c04c7da31
esheltavorax <eshel.tavor@axonius.com>
2025-01-05 10:20:41 +0200

    more fixes 2

--- Network.java
+++ Network.java
@@ -43,8 +43,8 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        if (userCount < users.length && getUser(name.toLowerCase()) == null) {
-            users[userCount] = new User(name.toLowerCase());
+        if (userCount < users.length && getUser(name) == null) {
+            users[userCount] = new User(name);
             userCount++;
             return true;
         }
